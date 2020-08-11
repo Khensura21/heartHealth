6d647b3c6cb4333e614b49e7a5b32fc7f81a75f7
Darian Nwankwo <nwankwodarian@gmail.com>
2020-08-10 22:39:42 -0400

    its fixed bro

--- backend/routes/processData.js
+++ backend/routes/processData.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var http = require('http');
 var axios = require("axios");
+var celery = require("celery-node");
 var router = express.Router();
 var sqlite3 = require("sqlite3").verbose();
+var client = celery.createClient(
+  "redis://localhost:6379/0",
+  "redis://localhost:6379/0"
+);
 db = new sqlite3.Database("./database/heart_health.db");
+const heartDiseasePrediction = client.createTask(
+  "inference-workers.tasks.heart_disease_prediction"
+);
+
 
 
 router.get('/', (req, res) => {
@@ -13,36 +22,13 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     // const { name, age, sex, chestPainVal, restbps, cholesterol } = req.body.data
     // res.send("This is the post request from processData route!")
-    console.log(req.body.data)
-    // //   //Gather request body to populate db command
-    // // db.prepare("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?)");
-    // db.serialize(function () {
-    //     db.run("INSERT INTO users VALUES (age, 'name', 9, 0, 2, 155, 253)");
-    //     db.each("SELECT * FROM users", (err, row) => {
-    //         if (err) console.log(err);
-    //         console.log(`Name: ${row.name}`);
-    //     })
-    // });
-
-    // db.close();
-    // const newReq = http.request({
-    //     host: '127.0.0.1',
-    //     port: 3000,
-    //     method: 'POST',
-    //     path: '/inference'
-    //   }, (res) => {
-    //     console.log("Made request...");
-    //     res.on("data", (chunk) => {
-    //         console.log(chunk);
-    //     });
-    //     console.log("End of request");
-    //   });
-    axios.post("http://localhost:3000/inference", {
-      data: req.body.data
-    })
-      .then((req2, res) => res.send(req2.body.heartPrediction));
-    //   res.redirect("https://epsn.com")
-
+    const heartData =  [
+      [...Object.values(req.body.data)].slice(1)
+    ];
+    const result = heartDiseasePrediction.applyAsync(heartData);
+    result.get().then(responseData => {
+      res.send(JSON.stringify({heartPrediction: responseData}));
+    });
 });
 //db.run(`INSERT INTO users VALUES (${name}, ${age}, ${sex}, ${chestPainVal}, ${restbps}, ${cholesterol})`);
 //res.send("Data was sucessfully submitted to database is the post request from processData route!")
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,7 @@
 const express = require("express"),
-      celery = require("celery-node"),
       app = express(),
       expressWs = require('express-ws')(app),
       cors = require('cors'),
-      client = celery.createClient(
-        "redis://localhost:6379/0",
-        "redis://localhost:6379/0"
-      ),
       bodyParser = require('body-parser');
 
 var processDataRoute = require('./routes/processData');
@@ -15,41 +10,6 @@ const { urlencoded } = require("body-parser");
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-
-const heartDiseasePrediction = client.createTask(
-  "inference-workers.tasks.heart_disease_prediction"
-);
-
-
-//steps to get user its prediction
-// submit user data to the database (on this part, look into sql db tutorial!!)
-// redirect that user data to the inference endpoint
-// run inference, get data back from celery 
-// send prediction back from inference endpoint as a res.json
-// display that response on the frontend w/ react via component will mount
-//then your done w project +!
-
-app.post("/inference", (req, res) => {
-  // Information About The User: Age, Height, Weight, Cholestoral, Zinc
-  // Train a logistic regression classifier using age, sex, chest pain, resting
-  // blood pressure and serum cholestora
-  //console.log(req.body.data);
-  const heartData =  [
-    [...Object.values(req.body.data)].slice(1)
-  ];
-  console.log("Calling task...\n", heartData);
-  console.log(heartDiseasePrediction);
-  const result = heartDiseasePrediction.applyAsync(heartData);
-  console.log("About to retrieve results...");
-  //console.log(result.get().then());
-  result.get().then(responseData => {
-    console.log(responseData);
-    res.send(JSON.stringify({heartPrediction: responseData}));
-    //client.disconnect();
-  });
-
-})
 
  app.use('/api/processData', processDataRoute);
 
